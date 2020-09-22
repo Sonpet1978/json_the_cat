2c13d0ddb24afbc6976f734d86f0e5d16fdfef39
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-09-22 13:40:37 -0400

    add

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,29 @@
 'use strict;';
-//console.log('ddd');
+console.log('ddd');
 const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
 
-  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=${breedName}';
   request(url, (error, resp, body) => {
    console.log(body);
-  // console.log(typeof body);
+   console.log(typeof body);
 
     if (error) {
-      callback(`Failed to request details: ${error}`,null);
+      callback('Failed to request details: ${error}',null);
     }
 
     const data = JSON.parse(body);
-    //console.log(data);
+    console.log(data);
 
     const breed = data[0];
     if (breed) {
       callback(null,breed.description);
     } else {
-      callback(`Failed to find breed ${breedName}`,null);
+      callback('Failed to find breed ${breedName}',null);
     }
   });
 
 };
 
-module.exports = { fetchBreedDescription };
+module.exports = { fetchBreedDescription  };
